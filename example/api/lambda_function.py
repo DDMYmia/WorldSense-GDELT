893c1e7e6e9562552d6cad11becda58189eab91b
wey <wzy398848627@gmail.com>
2025-09-30 13:21:17 +1300

    update

--- example/api/lambda_function.py
+++ example/api/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello World from Lambd222a!')
+        'body': json.dumps('Hello World5555 from Lambd222a!')
     }
